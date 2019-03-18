7ef063d63adfec8147c0e19918839ddbcf27bb85
loganwohlers <lawohlers@gmail.com>
2019-03-18 10:15:04 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :genres
+  resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
--- db/migrate/20151230232412_create_posts.rb
+++ db/migrate/20151230232412_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :description
+      t.string :description
+      t.string :post_status
 
       t.timestamps null: false
     end
